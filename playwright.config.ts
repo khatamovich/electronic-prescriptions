80908c0b23e263c147838d303ed8fcd486e84c22
khatamovich <khatamovich@icloud.com>
2025-03-20 20:33:21 +0500

    Modified playwright.cofnig.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   use: {
     baseURL: process.env.baseURL,
     ...devices['Desktop Chrome'],
+    browserName: 'chromium',
     channel: 'chrome',
     ignoreHTTPSErrors: true,
     video: 'on',
