6633a5fa4d7ba8d52c4a74fcff6b45e28f49f8b4
khatamovich <khatamovich@icloud.com>
2025-03-21 23:06:21 +0500

    Added utility functions

---
+++ utils/getEnvVars.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+export const getEnvVars = (
+	variables: string[],
+	options: { useActiveEnv: boolean } = { useActiveEnv: true },
+): string[] => {
+	const env = process.env.ENV;
+
+	if (!process.env.ENV) throw new Error('Could not find ENV');
+
+	const { parsed } = !options?.useActiveEnv
+		? dotenv.config()
+		: dotenv.config({ path: `.env.${env}` });
+
+	const result: string[] = [];
+
+	parsed &&
+		variables.forEach((variable) => {
+			variable = variable.toUpperCase();
+			result.push(parsed[variable]);
+		});
+
+	return result;
+};
---
+++ utils/getPath.ts
@@ -0,0 +1,3 @@
+import path from 'path';
+
+export const getPath = (relativePath: string) => path.join(relativePath);
